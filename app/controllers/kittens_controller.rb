a8708d1454be0c1b41c6b156abdede51767ec208
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-20 16:01:58 +0600

    get for index and show working with json data

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,11 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @kittens }
+      format.json { render :json => @kittens }
+    end
   end
 
   def new
@@ -20,6 +25,11 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @kitten }
+      format.json { render :json => @kitten }
+    end
   end
 
   def edit
